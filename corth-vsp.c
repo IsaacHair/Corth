003b68b6c39e9077b98001bac6350bc5fc013831
Isaac Hair <isaacmhair@gmail.com>
2020-04-10 15:40:07 -0700

    should be fixed now

--- corth-vsp.c
+++ corth-vsp.c
@@ -1,4 +1,3 @@
-HEAD
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -218,4 +217,4 @@ int main(int argc, char** argv) {
   //  bracket(b, t);
   
   return 0;
-}0429dc0b3fbacc6d8aa424f4d6fc118347186ce9
+}
