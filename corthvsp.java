27a451beba8b116cfbcc008d4b498394c6f61aa9
Isaac Hair <isaacmhair@gmail.com>
2020-04-29 16:58:34 -0700

    got the basic basics of java down

--- corthvsp.java
+++ corthvsp.java
@@ -1,30 +1,25 @@
 import java.io.*;
-import java.util.Scanner;
-public class corthvsp {
-    public static void main(String[] args) {
-	try {
-	    File source = new File(args[0]);
+public class corthvsp {  
+    public static void main(String[] args) {  
+	if (args.length != 2) {
+	    System.out.print("Error 0x01\n"+
+			     "usage: java cvm <source> <target>\n");
+	    System.exit(0x01);
 	}
-	catch (FileNotFoundException ex) {
-	    System.out.print("crap\n");
+	try (InputStream sfd = new FileInputStream(args[0]);
+	     OutputStream tfd = new FileOutputStream(args[1]);) {
+	    String s = ": bitch\n";
+	    byte[] arr = new byte[s.length()];
+	    for (int i = 0; i < s.length(); i++)
+		arr[i] = (byte)s.charAt(i);
+	    for (int c = sfd.read(); c != -1; c = sfd.read()) {
+		tfd.write(c);
+		tfd.write(arr);
+	    }
 	}
-	Scanner sourcescan = new Scanner(source);
-	for (int i = 0; sourcescan.hasNextLine(); i++)
-	    System.out.print("L"+i+sourcescan.nextLine());
-	System.out.print("hola\n");
-	random me = new random(669);
-	me.doSomething();
+	catch (Exception e) {
+	    System.out.println("Error 0x02\nunable to open files");
+	    System.exit(0x02);
+	}
     }
-}
-
-class random {
-    private int val;
-    public random(int num) {
-	System.out.print("hola again\n");
-	this.val = num;
-    }
-    void doSomething() {
-	System.out.print("nigga\n");
-	System.out.print(this.val+"\n");
-    }
-}
+}  
