9f44374ab8eb0fdf98cc2bd316aad5312c799133
Isaac Hair <isaacmhair@gmail.com>
2020-05-05 00:34:19 -0700

    beginning to write the compiler

--- corthvsp.java
+++ corthvsp.java
@@ -2,31 +2,11 @@ import java.io.*;
 import java.util.*;
 public class corthvsp {  
     public static void main(String[] args) {  
-	if (args.length != 2) {
-	    System.out.print("Error 0x01\n"+
-			     "usage: java cvm <source> <target>\n");
+        if (args.length != 2) {
+	    System.out.println("Error 0x01\n"+
+			       "usage: java corthvsp <source> <target>");
 	    System.exit(0x01);
 	}
-	try (InputStream sfd = new FileInputStream(args[0]);
-	     OutputStream tfd = new FileOutputStream(args[1]);) {
-	    String s = ": bitch\n";
-	    byte[] arr = new byte[s.length()];
-	    for (int i = 0; i < s.length(); i++)
-		arr[i] = (byte)s.charAt(i);
-	    for (int c = sfd.read(); c != -1; c = sfd.read()) {
-		tfd.write(c);
-		tfd.write(arr);
-	    }
-	}
-	catch (Exception e) {
-	    System.out.println("Error 0x02\nunable to open files");
-	    System.exit(0x02);
-	}
-	ArrayList<Integer> exa = new ArrayList<Integer>();
-	exa.add(1);
-	exa.add(2);
-	exa.add(3);
-	for (int i = 0; i < exa.size(); i++)
-	    System.out.println(exa.get(i));
+	
     }
 }  
