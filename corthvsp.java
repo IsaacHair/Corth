1c0a13eb55d3d0bc87af509bf12f898f9d5304dc
Isaac Hair <isaacmhair@gmail.com>
2020-05-11 01:02:44 -0700

    laying out the groundwork for the objects

--- corthvsp.java
+++ corthvsp.java
@@ -1,25 +1,27 @@
 import java.io.*;
 import java.util.*;
 public class corthvsp {  
-    public static void main(String[] args) {  
+    public static void main(String[] args) throws Exception {  
         if (args.length != 2) {
 	    System.out.println("Error 0x01\n"+
 			       "usage: java corthvsp <source> <target>");
 	    System.exit(0x01);
 	}
-	test obj = new test();
-	obj.f2();
-	obj.a.b.f2();
+	InputStream source = new FileInputStream(args[0]);
+	ArrayList<line> program = new ArrayList<line>();
+        int c, i, pos;
+	pos = 0;
+	while (source.available() > 0) {
+	    i = 0;
+	    for ((c = source.read()) != '\n' && c != -1)
+		i++;
+	    
+	source.close();
     }
 }
 
-class test {
-    private void f1() {
-	System.out.print("f1\n");
+class line {
+    public void line() {
+
     }
-    class a { class b { public void f2() {
-	f1();
-        super.super.f1();
-	System.out.println("f2");
-    } } }
 }
